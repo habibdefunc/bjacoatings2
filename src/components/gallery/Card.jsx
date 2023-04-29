144407e708146ded3f537e607ea4d754fe8652b8
aeez <makramfais@gmail.com>
2023-04-29 15:48:35 +0700

    fix carousel

--- src/components/gallery/Card.jsx
+++ src/components/gallery/Card.jsx
@@ -9,7 +9,7 @@ import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper";
 
-const cards = [
+const card1 = [
     {
         id: 1,
         name: "PT. Cemerlang Samudera Kontrindo",
@@ -21,17 +21,18 @@ const cards = [
         image5: "/assets/images//PT_CEMERLANG_SAMUDRA_KONTRINDO/File_008.png",
         image6: "/assets/images//PT_CEMERLANG_SAMUDRA_KONTRINDO/File_013.png",
     },
+];
+
+const card2 = [
     {
         id: 2,
         name: "PT. Ceppa Indonesia",
         explain: "Menggunakan epoxy lantai (SL), dengan ketebalan 2000 micron",
         image1: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
-        image2: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
-        image3: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
-        image4: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
-        image5: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
-        image6: "/assets/images/PT_CEPPA_INDONESIA_(PABRIK_WALET)/File_000.png",
     },
+];
+
+const card3 = [
     {
         id: 3,
         name: "PT. SPA SIBOLGA",
@@ -39,22 +40,20 @@ const cards = [
             "Menggunakan material ucrete (yang kerasnya sudah seperti k350 dan sudah memenuhi standart foodgrade), dengan ketebalan 6mm",
         image1: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_000.png",
         image2: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_001.png",
-        image3: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_000.png",
-        image4: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_001.png",
-        image5: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_000.png",
-        image6: "/assets/images/PT_SPA_SIBOLGA_(PABRIK_IKAN_TUNA)/File_001.png",
     },
+];
+
+const card4 = [
     {
         id: 4,
         name: "PT. SPA SIBOLGA (Cold Storage)",
         explain: "Menggunakan epoxy mortar, dengan ketebalan 9mm",
         image1: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_002.png",
         image2: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_003.png",
-        image3: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_002.png",
-        image4: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_003.png",
-        image5: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_002.png",
-        image6: "/assets/images/PT_SPA_SIBOLGA_(COLD_STORAGE)/File_003.png",
     },
+];
+
+const card5 = [
     {
         id: 5,
         name: "PT. Sri Makmur Abadi",
@@ -67,6 +66,9 @@ const cards = [
         image5: "/assets/images/PT_SRI_MAKMUR_ABADI_(PABRIK_SANTAN)/File_004.png",
         image6: "/assets/images/PT_SRI_MAKMUR_ABADI_(PABRIK_SANTAN)/File_005.png",
     },
+];
+
+const card6 = [
     {
         id: 6,
         name: "SMKN 1 TANJUNG MORAWA",
@@ -76,9 +78,10 @@ const cards = [
         image2: "/assets/images/SMKN1_TANJUNG_MORAWA/File_002.png",
         image3: "/assets/images/SMKN1_TANJUNG_MORAWA/File_003.png",
         image4: "/assets/images/SMKN1_TANJUNG_MORAWA/File_004.png",
-        image5: "/assets/images/SMKN1_TANJUNG_MORAWA/File_000.png",
-        image6: "/assets/images/SMKN1_TANJUNG_MORAWA/File_002.png",
     },
+];
+
+const card7 = [
     {
         id: 7,
         name: "Yayasan Pesantren Raudhatul Hasanah",
@@ -103,7 +106,316 @@ const Card = () => {
             <div className="mx-auto mt-6 w-1/2 border-b-2 border-green-700/80 lg:w-1/6"></div>
 
             <div className=" mt-16 grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                {cards.map((card) => (
+                {card1.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image2}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image3}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image4}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image5}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image6}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card2.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card3.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image2}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card4.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image2}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card5.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image2}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image3}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image4}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image5}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image6}
+                                    className="slide-img object-cover"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card6.map((card) => (
+                    <div
+                        className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
+                        key={card.id}
+                    >
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded-lg"
+                        >
+                            <SwiperSlide>
+                                <img
+                                    src={card.image1}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image2}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image3}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src={card.image4}
+                                    className="slide-img transform object-cover transition-all duration-1000 hover:scale-110"
+                                    alt=""
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+
+                        <div className="my-6 mx-4">
+                            <h3 className="mb-3 text-2xl font-bold">
+                                {card.name}
+                            </h3>
+                            <p>{card.explain}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {card7.map((card) => (
                     <div
                         className=" grid grid-cols-1 rounded-lg bg-gray-300 sm:grid-cols-1 lg:grid-cols-1"
                         key={card.id}
